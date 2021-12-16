bafed29b6dba015cae74f79193c0c9b0cf75df94
TomieSan <santosaliyahh@gmail.com>
2021-12-16 20:23:14 +0800

    commit

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://backend-myfirstreact.herokuapp.com/api/v1/employeess',
+    baseURL: 'https://backend-myfirstreact.herokuapp.com/api/v1/employees',
     headers:{
         'Conent-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
